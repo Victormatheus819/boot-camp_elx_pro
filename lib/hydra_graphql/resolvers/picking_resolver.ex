b4567fdb579bbe0ad2e3100fc456dba4e7e176de
VictorMatheus819 <victormatheus819@gmail.com>
2021-12-14 14:41:11 -0300

    feat: handling with graphql

---
+++ lib/hydra_graphql/resolvers/picking_resolver.ex
@@ -0,0 +1,13 @@
+defmodule HydraGraphql.Resolvers.PickingResolver do
+
+  def create_picking(_parent, params,_resolution)do
+    IO.inspect params
+  #     %{
+  #   products: [
+  #     %{product: "123", stores: ["123", "123123"]},
+  #     %{product: "123", stores: ["123", "123123"]}
+  #   ]
+  # }
+    {:ok,"We received the payload ,and we are going to create all orders !"}
+  end
+end
