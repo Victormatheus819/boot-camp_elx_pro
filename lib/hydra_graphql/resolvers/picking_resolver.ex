8c6b748dc297eb3a1bd1ad4f7d17fe82a5e83b79
VictorMatheus819 <victormatheus819@gmail.com>
2021-12-14 15:55:27 -0300

    feat : send products to kafka

--- lib/hydra_graphql/resolvers/picking_resolver.ex
+++ lib/hydra_graphql/resolvers/picking_resolver.ex
@@ -1,4 +1,5 @@
 defmodule HydraGraphql.Resolvers.PickingResolver do
+  alias Hydra.Pickings
 
   def create_picking(_parent, params,_resolution)do
     IO.inspect params
@@ -8,6 +9,7 @@ defmodule HydraGraphql.Resolvers.PickingResolver do
   #     %{product: "123", stores: ["123", "123123"]}
   #   ]
   # }
+ Task.async(fn ->Pickings.create_picking(params) end)
     {:ok,"We received the payload ,and we are going to create all orders !"}
   end
 end
